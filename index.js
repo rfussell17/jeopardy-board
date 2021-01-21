5d879058232ba43775b59500143fec43a9f0a09d
Robin Fussell <rfussell17@gmail.com>
2021-01-21 11:07:03 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+async function getCategories(count, offset){
+  let res = await fetch(`https://jservice.io/api/categories?count=${count}&offset=${offset}`)
+  let data = await res.json();
+  return data
+}
+
+function getCategoryHtml(category) {
+  return `
+      <div class="my-category-title">${category.title}</div>
+      
+      <div class="my-category-clue" style="grid-row-start: 2">$200</div>
+      <div class="my-category-clue" style="grid-row-start: 3">$400</div>
+      <div class="my-category-clue" style="grid-row-start: 4">$600</div>
+      <div class="my-category-clue" style="grid-row-start: 5">$800</div>
+      <div class="my-category-clue" style="grid-row-start: 6">$1000</div>
+  `
+}
+
+getCategories(6,5).then(categories => {
+  console.log(categories)
+  document.body.innerHTML = `<div class="board">
+      ${categories.map(getCategoryHtml).join('')}
+  </div>`
+})
+
